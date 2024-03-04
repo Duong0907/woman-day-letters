eddc0a0f89d5d80c78f5594c539054400f74bea1
emxi <emxi.dev@gmail.com>
2024-03-04 13:01:53 +0700

    update mesage

--- screen2.js
+++ screen2.js
@@ -1,3 +1,40 @@
+const messageQueue = [
+    `Chúc các bạn nữ ngày 8/3 luôn xinh đẹp như hoa, rạng rỡ như nắng và ngọt ngào như mật. Happy Women's Day!!!!`,
+    `Chúc tất cả chị em ZaloPay không chỉ có 1 ngày 8/3 mà mỗi ngày đều vui vẻ, trẻ trung, xinh đẹp nhé!`,
+    `Chúc các chị/em có một ngày 8/3 thật ý nghĩa, được mọi người yêu thương, tiền đầy túi, tình đầy tim.
+    Happy Women's Day`,
+    `Văn thì em chẳng có đâu
+    Thơ thì... một chút đôi câu gọi là
+    
+    "Hôm nay mùng tám tháng ba
+    Là ngày gửi những lời ca đến người
+    Gửi nửa thế giới xinh tươi
+    Luôn luôn vui vẻ rạng ngời như hoa
+    Yêu đời hát những khúc ca
+    Nửa kia thế giới nghe là mê say
+    Giấc mơ thì hãy cứ bay
+    Bay cao cao vút đến ngày thành công
+    Mong vạn điều ước chờ mong
+    Sẽ sớm chẳng phải ngóng trông đợi chờ
+    Thôi thì cứ mộng cứ mơ
+    (Cho) nửa kia thế giới ngẩn ngơ ngắm nhìn."
+    
+    _Chúc Mừng 8/3 các chị em ZaloPay_
+    _From PTO-Platform with Love <3_`,
+    `Chúc mừng ngày của những người phụ nữ đáng quý! Mong bạn luôn được tràn đầy niềm vui, sức khỏe và thành công trong cuộc sống và sự nghiệp.`,
+    `Chúc bạn ngày Quốc tế Phụ nữ thật vui vẻ và đầy ý nghĩa! Hãy tiếp tục truyền cảm hứng và là nguồn động viên cho mọi người xung quanh nhé.`,
+    `Chúc các chị em ZaloPay thật vui vẻ và rạng rỡ trong ngày 8/3
+    Chúc chị em ngày càng xinh đẹp, công việc thuận lợi và sớm đạt được điều mình muốn. 
+    Về tình cảm, mình xin chúc cho chị em nào đang yêu sẽ ngày càng nồng thắm, ai đang có crush thì sớm được tỏ tình, còn không nữa thì sớm tìm được nửa kia.
+    Happy Woman Day!`,
+    `Chúc các bạn nữ hãy luôn là những đoá hoa xinh tươi của VNG nói chung và ZaloPay nói riêng nhé 🥰✨☀️🌻`,
+    `Mùng 8 - 3 không chúc các chị em cái gì, chỉ mong các chị em luôn độc lập - tự do - hạnh phúc. Độc lập thì đường hoàng, không phải dựa dẫm vào ai thì có tự do, mà có tự do, là có hạnh phúc.`,
+    `Chúc chị em luôn xinh đẹp, tình đầy tim, tiền đầy túi`,
+    `Chúc các chị em phụ nữ ZaloPay có một ngày 8/3 thật ý nghĩa và hạnh phúc bên gia đình và bạn bè của mình nhé.`,
+    `Gửi lời chúc riêng đến các chị em bạn bè ZaloPay mãi tươi trẻ và nhiệt huyết <3`,
+    `Chúc các bạn nữ ZaloPay một ngày 8/3 vui vẻ, luôn vui tươi, xinh đẹp và yêu đời như mọi ngày. Mặc kệ những lời đàm tiếu xung quanh, vì em đẹp nhất khi là chính em.`,
+];
+
 const LETTER_WIDTH = 60;
 const LETTER_HEIGHT = 40;
 
@@ -5,8 +42,8 @@ const MAX_VY = 3;
 const MIN_VY = 1.5;
 const MAX_VX = 1;
 const MIN_VX = -1;
-const TIME_STEP = 1000;
-const RENDER_EACH_TIME = 2;
+const TIME_STEP = 800;
+const RENDER_EACH_TIME = 1;
 const MARGIN = 100;
 const NUMBER_OF_VIDEO_LETTER = 5;
 const LETTER_IMAGE = '';
@@ -91,7 +128,8 @@ window.onload = () => {
         } else {
             letterQueue.push(
                 new Letter('message', '', screenWidth, () => {
-                    dialogInnerMessage.innerText = 'MESSAGE';
+                    dialogInnerMessage.innerText = messageQueue[0];
+                    moveFirstToEnd(messageQueue);
                     dialogInner.classList.remove('video');
                     dialogInnerVideo.innerHTML = '';
                 })
